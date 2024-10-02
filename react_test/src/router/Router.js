f9f0639e588afd44e2e7a044dbbd82a3a8c58ccb
taiki toyoda <toyoda@honeycomb-lab.co.jp>
2024-10-02 17:49:28 +0900

    routerの階層の変更

--- react_test/src/router/Router.js
+++ react_test/src/router/Router.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import MainPage from '../../containers/pages/main.js';
+import MainPage from '../containers/pages/main.js';
 
 export const Router = () => {
   return (
