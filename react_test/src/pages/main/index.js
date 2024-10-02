d3d69fc30a035c020a30a1cc8f1cc9135091be7f
taiki toyoda <toyoda@honeycomb-lab.co.jp>
2024-10-02 17:20:59 +0900

    stylusを使用できるように変更

--- react_test/src/pages/main/index.js
+++ react_test/src/pages/main/index.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
+import '../../styles/main.styl';
 
 function Main() {
     return (
-        <div className="App">
+        <div className="main">
           test
         </div>
     );
